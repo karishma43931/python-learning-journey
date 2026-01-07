bd30cdb98cbf8c22640f793b5fef4bf6f43c54b8
Karishma Shaik <karishmakarishma43931@gmail.com>
2026-01-08 02:25:18 +0530

    comminting day1 basic hands-on programs

---
+++ Day01_Basics/name_and_age.py
@@ -0,0 +1,8 @@
+'''
+Day 1 – Python Basics
+Difficulty: Easy
+Program: Print name and age
+'''
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello  {name}, your {age} years old.")
--- Day1Basics/printingname.py
+++
@@ -1,4 +0,0 @@
-# Printing Name and Age
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-print(f"Hello  {name}, your {age} years old. ")
